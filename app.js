e6e74724425f2f4ffaf905f44cac621b00cba297
Rodrigo Vargas <Rodrigo Vargas>
2016-12-03 21:26:48 -0200

    Added list component

--- app.js
+++ app.js
@@ -1,22 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/*const element = {
-  type: 'h1',
-  className: 'test',
-  props:{    
-    children: 'Hello, world!'
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listItems: [],
+      newItemDescription: ""
+    }
+
+    // This binding is necessary to make `this` work in the callback
+    this.add = this.add.bind(this);
+    this.onNewItemChange = this.onNewItemChange.bind(this);
+  }
+
+  add() {
+    var listItems = this.state.listItems;
+    var length = this.state.listItems.length + 1;
+    listItems.push({ id : length, name : this.state.newItemDescription })
+    this.setState( { listItems : listItems } );
+    this.state.newItemDescription = "";
+  }
+
+  onNewItemChange(event){
+    this.setState({newItemDescription: event.target.value});
+  }
+
+  componentDidMount() {
+    var listItems = [];
+    listItems.push({ id : 1, name: this.props.listItems });
+    this.setState({ listItems : listItems });
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render(){
+    console.log(this.state.listItems);
+    const listItems = this.state.listItems.map((item) =>
+      <li key={item.id}>
+        { item.name }
+      </li>
+    );
+
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <ul>
+          { listItems}
+        </ul>
+
+        <input value={this.state.newItemDescription} onChange={this.onNewItemChange} />
+
+        <a onClick={this.add}>Add Item</a>
+
+      </div>
+    );
   }
 }
-*/
 
 function H1 (props){
   return <h1>Hello, {props.name}</h1>;
 }
 
-const element = <H1 name="Rodrigo Vowqdhsafjkhsadfsdargas" />;
+const element = <H1 name="Rodrigo Vargas 2" />;
 
 ReactDOM.render(
-  element,
+  <List listItems='Teste' />,
   document.getElementById('root')
 );
+
